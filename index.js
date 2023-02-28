716170a551fa7bf7a0ca9fa3bdffa41919f44b2d
Prnzdiamond <oseahumenagboifoh@gmail.com>
2023-02-28 16:58:49 +0100

    finished home page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById("mnu");
-
+var ul = document.getElementById("menu");
     function dropdownmenu() {
        menu.classList.toggle("responsive")
+       ul.classList.toggle("ulrep")
     }
